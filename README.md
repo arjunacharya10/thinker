d8741680645307f4f8d84d6820ee379306994a9d
Arjun Acharya <arjunchangeeacharya1998@gmail.com>
2024-03-18 10:23:05 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Brains storming app using tldraw and electron. 
+
+Clone repo > run npm install > run npm run make > A new "out" Folder will be created in which the application will be present.
+
+NOTE: THis uses local storage, so if you delelte the app all data will be gone.
+
+Test
---
+++ src/app.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import { createClient } from '@supabase/supabase-js'
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
+import Home from './home'
+import './app.css';
+
+export const supabase = createClient('<YOUR PUBLIC URL>', '<YOUR ANON KEY>')
+
+export default function App() {
+  const [session, setSession] = useState(null)
+
+  useEffect(() => {
+	if(!session){
+		supabase.auth.getSession().then(({ data: { session } }) => {
+		localStorage.setItem('thinker-session',session)
+		setSession(session)
+		})
+	}
+
+	const {
+	  data: { subscription },
+	} = supabase.auth.onAuthStateChange((_event, session) => {
+	  localStorage.setItem('thinker-session',session)		
+	  setSession(session)
+	})
+
+	return () => subscription.unsubscribe()
+  }, [])
+
+if (!session) {
+	return (
+		<div style={{display:'flex', flexGrow:1, width:'40vh', height:'full', flexDirection:'column'}}>
+			<h1 style={{textAlign:'center'}}>Welcome to Thinker!</h1>
+			<Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />
+		</div>
+	)
+  }
+  else {
+	return (<Home supabaseClient={supabase}/>)
+  }
+}
